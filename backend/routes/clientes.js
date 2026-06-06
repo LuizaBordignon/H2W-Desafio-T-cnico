a6f66ec5b75704069e4c7a70114e1eae455efa9a
LuizaNote <luizabordignonmoro@gmail.com>
2026-06-06 16:41:36 -0300

    melhorias

--- backend/routes/clientes.js
+++ backend/routes/clientes.js
@@ -8,7 +8,8 @@ router.get('/', async (req, res) => {
         const resultado = await pool.query('SELECT * FROM clientes ORDER BY id ASC');
         res.json(resultado.rows);
     }catch (erro){
-        res.status(500).json({ messagem: 'Erro ao buscar clientes'});
+        console.error(erro.message);
+        res.status(500).json({ messagem: 'Erro ao buscar cliente'});
     }
 });
 
@@ -16,6 +17,9 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req,res) => {
     const {nome,email,telefone} = req.body;
+    if (!nome || !email || !telefone) {
+        return res.status(400).json({ mensagem: 'Todos os campos são obrigatórios' });
+    }
     try{
         const resultado = await pool.query(
             'INSERT INTO clientes (nome, email, telefone) VALUES ($1, $2, $3) RETURNING *', 
@@ -23,6 +27,7 @@ router.post('/', async (req,res) => {
         );
         res.status(201).json(resultado.rows[0]);
     } catch (erro) {
+        console.error(erro.message);
         res.status(500).json({ mensagem: 'Erro ao criar cliente'});
     }
 });
@@ -32,13 +37,22 @@ router.post('/', async (req,res) => {
 router.put('/:id', async (req,res) => {
     const { id } = req.params;
     const { nome, email, telefone } = req.body;
+    if (!nome || !email || !telefone) {
+    return res.status(400).json({ mensagem: 'Todos os campos são obrigatórios' });
+    }
     try{
         const resultado = await pool.query(
         'UPDATE clientes SET nome=$1, email=$2, telefone=$3 WHERE id=$4 RETURNING *',
         [nome, email, telefone, id]
         );
+
+        if (resultado.rows.length === 0) {
+            return res.status(404).json({ mensagem: 'Cliente não encontrado' });
+        }
+
         res.json(resultado.rows[0])
     }catch (erro){
+        console.error(erro.message);
         res.status(500).json({ mensagem: 'Erro ao editar cliente '});
     }
 });
@@ -49,8 +63,12 @@ router.delete('/:id', async (req,res) => {
     const { id } = req.params;
     try{
         await pool.query('DELETE FROM clientes WHERE id=$1', [id]);
+        if (resultado.rowCount === 0) {
+        return res.status(404).json({ mensagem: 'Cliente não encontrado' });
+        }
         res.json({ mensagem: 'Cliente removido '});
     } catch(erro){
+        console.error(erro.message);
         res.status(500).json({mensagem: 'Erro ao remover cliente'});
     }
 });
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const clientesRoutes = require('./routes/clientes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
